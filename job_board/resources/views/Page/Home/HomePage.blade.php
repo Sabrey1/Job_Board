24f34d9234ad30e0102519a22f5c38fb66adab84
Sabrey <limsabriey94@gmail.com>
2025-11-08 15:46:24 +0700

    Update

---
+++ job_board/resources/views/Page/Home/HomePage.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+   <div  style="margin-top: -100px">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
