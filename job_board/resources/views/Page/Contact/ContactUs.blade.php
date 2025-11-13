f855013e2fd591d60e80c8302b5bbdc9104510ea
Sabrey1 <limsabriey94@gmail.com>
2025-11-13 14:47:16 +0700

    Update

--- job_board/resources/views/Page/Contact/ContactUs.blade.php
+++ job_board/resources/views/Page/Contact/ContactUs.blade.php
@@ -1,10 +1,19 @@
 <x-app-layout>
    <div style="z-index: -1">
         <div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-gray-500 overflow-hidden shadow-sm ">
                 <div class="p-6 text-gray-900"  style="max-width: 1200px; width: 100%;  margin: 0 auto; ">
                     <p>{{ __("ទាក់ទងមកយើងខ្ញុំ") }}</p>
                     <p> {{ __("Contact us") }}</p>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm ">
+                <div class="p-6 text-gray-900"  style="max-width: 1200px; width: 100%;  margin: 0 auto; ">
+                   <div class="grid grid-cols-6 gap-6">
+                        <div class="col-span-6">01</div>
+
+                        <div>09</div>
+                    </div>
                 </div>
             </div>
         </div>
